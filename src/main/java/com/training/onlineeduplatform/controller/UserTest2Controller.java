127cca448bfc89e8d762baeb6d12974386727f45
TankTan-hei <1074567974@qq.com>
2019-12-29 23:23:37 +0800

    添加@controller

--- src/main/java/com/training/onlineeduplatform/controller/UserTest2Controller.java
+++ src/main/java/com/training/onlineeduplatform/controller/UserTest2Controller.java
@@ -1,9 +1,13 @@
 package com.training.onlineeduplatform.controller;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * Created on 2019/12/29.
  *
  * @author Youjie Xia
  */
+@Controller
 public class UserTest2Controller {
+
 }
